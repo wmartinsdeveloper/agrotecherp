da67db5a787bd1c71a70ede20571130db0de27d6
wmartinsdeveloper <wmartinsdeveloper@gmail.com>
2024-08-19 18:18:10 -0300

    teste

--- src/main/java/com/agrotech/erp/entities/produto.java
+++ src/main/java/com/agrotech/erp/entities/produto.java
@@ -2,6 +2,8 @@ package com.agrotech.erp.entities;
 
 import java.util.Date;
 
+import com.agrotech.erp.entities.enums.unidadePeso;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
